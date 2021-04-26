a5143f8244cc9b905b08dbcb3e68af8eaf0e359b
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-04-26 18:02:43 +0300

    Add PicturesService ViewModels

---
+++ ViceIO/Services/ViceIO.Services/PicturesService.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+using ViceIO.Data.Common.Repositories;
+using ViceIO.Data.Models;
+using ViceIO.Web.ViewModels.Pictures;
+
+namespace ViceIO.Services
+{
+    public class PicturesService : IPicturesService
+    {
+        private readonly IDeletableEntityRepository<Picture> picturesRepository;
+
+        public PicturesService(IDeletableEntityRepository<Picture> picturesRepository)
+        {
+            this.picturesRepository = picturesRepository;
+        }
+
+        public async Task CreateAsync(CreatePictureInputModel input, string userId)
+        {
+            var picture = new Picture()
+            {
+                AddedByUserId = userId,
+                CategoryId = input.CategoryId,
+                Name = input.Name,
+                Url = input.Url,
+                SourceUrl = input.SourceUrl,
+
+            };
+
+            await this.picturesRepository.AddAsync(picture);
+            await this.picturesRepository.SaveChangesAsync();
+        }
+    }
+}
---
+++ ViceIO/Web/ViceIO.Web.ViewModels/PicturesCategories/PicturesCategoriesModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ViceIO.Web.ViewModels.PicturesCategories
+{
+    public class PicturesCategoriesModel
+    {
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+
+        [Required]
+        [Url]
+        public string ImageUrl { get; set; }
+    }
+}
