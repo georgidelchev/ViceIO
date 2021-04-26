1fe1041f666a94d88ec49d06654093e0c45c6818
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-04-26 19:19:31 +0300

    Add Pictures/All.cshtml PicturesController/All

--- ViceIO/Services/ViceIO.Services/IPicturesService.cs
+++ ViceIO/Services/ViceIO.Services/IPicturesService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 
 using ViceIO.Web.ViewModels.Pictures;
 
@@ -7,5 +8,13 @@ namespace ViceIO.Services
     public interface IPicturesService
     {
         Task CreateAsync(CreatePictureInputModel input, string userId);
+
+        IEnumerable<GetPictureBaseViewModel> GetAll();
+
+        GetPictureBaseViewModel GetRandom();
+
+        int GetCount();
+
+        GetPictureBaseViewModel GetById(int pictureId);
     }
 }
