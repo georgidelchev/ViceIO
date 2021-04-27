b2111dceb52d367ea952ed2dade3986f73b14f5f
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-04-27 17:11:58 +0300

    Add Random.cshtml

--- ViceIO/Web/ViceIO.Web/Controllers/PicturesController.cs
+++ ViceIO/Web/ViceIO.Web/Controllers/PicturesController.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using ViceIO.Services;
 using ViceIO.Web.ViewModels.Pictures;
 
 namespace ViceIO.Web.Controllers
 {
-    public class PicturesController:Controller
+    public class PicturesController : Controller
     {
         private readonly IPicturesService picturesService;
         private readonly IPicturesCategoriesService picturesCategoriesService;
@@ -36,6 +36,29 @@ namespace ViceIO.Web.Controllers
             {
                 Categories = this.picturesCategoriesService.GetAll(),
             };
+
+            return this.View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreatePictureInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(input);
+            }
+
+            var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            await this.picturesService.CreateAsync(input, userId);
+
+            return this.Redirect("/Pictures/All");
+        }
+
+        [HttpGet]
+        public IActionResult Random()
+        {
+            var viewModel = this.picturesService.GetRandom();
 
             return this.View(viewModel);
         }
