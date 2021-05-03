1b73fe89109140e569d2cf5aeae6df4438347b32
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-05-03 18:00:01 +0300

    Add test

--- ViceIO/Web/ViceIO.Web.ViewModels/Riddles/CreateRiddleInputModel.cs
+++ ViceIO/Web/ViceIO.Web.ViewModels/Riddles/CreateRiddleInputModel.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ViceIO.Web.ViewModels.Riddles
 {
    public class CreateRiddleInputModel
     {
+        [Required]
+        [MaxLength(2000)]
+        public string Content { get; set; }
     }
 }
