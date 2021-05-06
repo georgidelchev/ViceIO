da9846b47d9dd652f90355acbf6d934a8fe073b5
Aleksandra Toncheva <67632893+tonchevaAleksandra@users.noreply.github.com>
2021-05-06 17:49:02 +0300

    Add AdminController.cs

---
+++ ViceIO/Web/ViceIO.Web/Controllers/AdminController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ViceIO.Web.Controllers
+{
+    [Authorize("Admin")]
+
+    public class AdminController : Controller
+    {
+    }
+}
