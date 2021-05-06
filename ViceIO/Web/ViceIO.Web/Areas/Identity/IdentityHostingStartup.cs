ec5f9cef22f2c2acf557f6215bfe494e94dfcd8d
george <alienguymc@gmail.com>
2021-05-06 17:51:15 +0300

    working on scaffolded login and register view

---
+++ ViceIO/Web/ViceIO.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ViceIO.Data;
+using ViceIO.Data.Models;
+
+[assembly: HostingStartup(typeof(ViceIO.Web.Areas.Identity.IdentityHostingStartup))]
+namespace ViceIO.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
